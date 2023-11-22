2d1fa9a45be45c96b0b6d59b72165556c53cb7a8
bronsoncl <64222679+bronsoncl@users.noreply.github.com>
2023-11-21 20:10:19 -0800

    Completed React Template

---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const routes = [];
+
+export const MyRoutes = () => {
+    <Router>
+        <Routes>
+            {routes.map( (route, index) => (
+                <Route
+                    key = {index}
+                    path = {route.path}
+                    exact = {route.exact}
+                >
+                    <route.Component />
+                </Route>
+            ))}
+        </Routes>
+    </Router>
+}
